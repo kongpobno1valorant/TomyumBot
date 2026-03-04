b114d83c61a99eeab0ba544c98112c72eb92839d
James <jiraphatnam2204@gmail.com>
2026-03-04 19:35:43 +0700

    bugfix connect

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 intents.voice_states = True
+intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 temp_channels = {}
@@ -19,6 +20,26 @@ async def on_ready():
 @bot.command()
 async def hello(ctx):
     await ctx.send(f'Hello {ctx.author.name}! 👋')
+
+@bot.command()
+async def createroom(ctx, *, name="Temp Room"):
+    if ctx.author.id in temp_channels:
+        await ctx.send("You already have a room! Use !deleteroom first.")
+        return
+    channel = await ctx.guild.create_voice_channel(name=name)
+    temp_channels[ctx.author.id] = channel.id
+    await ctx.send(f'✅ Room **{name}** created!')
+
+@bot.command()
+async def deleteroom(ctx):
+    if ctx.author.id not in temp_channels:
+        await ctx.send("You don't have a room.")
+        return
+    channel = ctx.guild.get_channel(temp_channels[ctx.author.id])
+    if channel:
+        await channel.delete()
+    del temp_channels[ctx.author.id]
+    await ctx.send("🗑️ Room deleted.")
 
 @bot.event
 async def on_voice_state_update(member, before, after):
